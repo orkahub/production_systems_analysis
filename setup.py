bc02fe5d927185f6455bf3ed2466aa47b11b7ca8
edwindt <dwindt.eduardo@gmail.com>
2020-08-22 10:25:09 -0500

    Remove Prints in lib

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'psapy',
   packages = ['psapy'],
-  version = '0.2',
+  version = '0.3',
   license='MIT',
   description = 'Production System Analysis',
   author = 'Orkahub Energy',
   author_email = 'orkahub@gmail.com',
   url = 'https://github.com/orkahub/production_systems_analysis',
-  download_url = 'https://github.com/orkahub/production_systems_analysis/archive/v_02.tar.gz',
+  download_url = 'https://github.com/orkahub/production_systems_analysis/archive/v_03.tar.gz',
   keywords = ['RESERVOIR', 'ENERGY', 'OIL', 'GAS', 'PRODUCTION', 'VLP', 'IPR'],
   install_requires=[
           'pandas',
